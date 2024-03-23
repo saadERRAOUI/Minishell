26c792822f43e541406db3977b186d87d58579e2
hichambouzid <bouzidhicham51@gmail.com>
2024-03-23 21:34:40 +0000

    PUSH-H-0.0.1

---
+++ minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hibouzid <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/23 18:02:20 by hibouzid          #+#    #+#             */
+/*   Updated: 2024/03/23 18:10:24 by hibouzid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include<unistd.h>
+#include<stdlib.h>
+
+int ft_strcmp(char *s1, char *s2);
+int ft_putstr_fd(char *s, int fd);
+int	ft_strlen(char *str);
+
+#endif
