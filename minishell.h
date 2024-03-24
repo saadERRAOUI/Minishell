9ab82ec55edfe8f83439b7906eb69433810922f9
Saad ERRAOUI <saaderraouiinfosup@gmail.com>
2024-03-24 03:38:04 +0000

    PUSH-S-0.0.0 : Add getToken function to the test.c file Minishell bundle

--- minishell.h
+++ minishell.h
@@ -3,21 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hibouzid <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: serraoui <serraoui@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 18:02:20 by hibouzid          #+#    #+#             */
-/*   Updated: 2024/03/23 18:10:24 by hibouzid         ###   ########.fr       */
+/*   Updated: 2024/03/24 03:25:31 by serraoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 #define MINISHELL_H
 
-#include<unistd.h>
-#include<stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int ft_strcmp(char *s1, char *s2);
-int ft_putstr_fd(char *s, int fd);
-int	ft_strlen(char *str);
+int     ft_strcmp(char *s1, char *s2);
+int     ft_putstr_fd(char *s, int fd);
+int     ft_strlen(char *str);
+char    *ft_strchr(const char *s, int c);
 
 #endif
--- test.c
+++ test.c
@@ -3,14 +3,113 @@
 /*                                                        :::      ::::::::   */
 /*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hibouzid <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: serraoui <serraoui@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 23:54:50 by hibouzid          #+#    #+#             */
-/*   Updated: 2024/03/15 23:55:05 by hibouzid         ###   ########.fr       */
+/*   Updated: 2024/03/24 03:35:08 by serraoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char    *ft_strchr(const char *s, int c)
+{
+        char    character;
+
+        character = (char) c;
+        if (character == '\0' && s)
+                return ((char *)(s + ft_strlen(s)));
+        while (*s)
+        {
+                if (*s == (char)character)
+                        return ((char *)s);
+                s++;
+        }
+        if (character == '\0')
+                return ((char *)s);
+        return (NULL);
+}
+/*
+	@AUTHOR	: Saad ERRAOUI
+	@PROTO	: int	getToken(char **ps, char *es, char **q, char **eq);
+	@DESC	: utility function that returns the next token (INT) in the parsed
+		string.
+	@DATE	: 24-03-2024
+*/
+int	getToken(char **ps, char *es, char **q, char **eq)
+{
+	char	*whitespaces;
+	char	*symbols;
+	char	*s;
+	int		ret;
+
+	whitespaces = " \t\r\v\n";
+	symbols = "<|>;&()";
+	s = (*ps);
+	while (s < es && ft_strchr(whitespaces, (*s)))
+		s++;
+	if (q)
+		(*q) = s;
+	ret = (*s);
+	/* //TODO : MAKE THIS CODE IN A SEPERATE FUNC - START */
+	switch(*s)
+	{
+		case 0:
+			break;
+		case '|':
+		case '(':
+		case ')':
+		case '&':
+		case ':':
+		case '<':
+			s++;
+			break;
+		case '>':
+			s++;
+			if (*s == '>')
+			{
+				ret = '+';
+				s++;
+			}
+			break;
+		default:
+			ret = 'a';
+			while (s < es && !ft_strchr(whitespaces, (*s)) && !ft_strchr(symbols, (*s)))
+				s++;
+			break;
+	}
+	/* //TODO : MAKE THIS CODE IN A SEPERATE FUNC - END */
+	if (eq)
+		(*eq) = s;
+	while (s < es && ft_strchr(whitespaces, (*s)))
+		s++;
+	(*ps) = s;
+	return (ret);
+}
+
 int main()
 {
+	char *ps = "  >>sad|test";
+	char *es = (char *)ps + 12;
+	char *eq;
+	char *q;
+	int token = getToken(&ps, es, &q, &eq);
+	printf("next token %i %c\n", token, token);
+
+	token = getToken(&ps, es, &q, &eq);
+	printf("next token %i %c\n", token, token);
+	
+	token = getToken(&ps, es, &q, &eq);
+	printf("next token %i %c\n", token, token);
 	return (0);
 }
