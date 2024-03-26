660b3188426f236e1dfa1696205aa0aa15331acc
hichambouzid <bouzidhicham51@gmail.com>
2024-03-26 01:12:39 +0000

    PUSH-H-005: add some cases on file echo & switch branch

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: serraoui <serraoui@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: hibouzid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 18:02:20 by hibouzid          #+#    #+#             */
-/*   Updated: 2024/03/24 03:25:31 by serraoui         ###   ########.fr       */
+/*   Updated: 2024/03/25 00:46:33 by hibouzid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
+#include <signal.h>
 
-int     ft_strcmp(char *s1, char *s2);
-int     ft_putstr_fd(char *s, int fd);
-int     ft_strlen(char *str);
-char    *ft_strchr(const char *s, int c);
+#include "./libft/libft.h"
+
+int ft_strcmp(char *s1, char *s2);
+// int     ft_putstr_fd(char *s, int fd);
+// int     ft_strlen(char *str);
+// char    *ft_strchr(const char *s, int c);
 
 #endif
